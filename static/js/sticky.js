e7b4b485f406f51638e9fc40dd161dcbc2c0ad39
Liang Sun <i@liangsun.org>
2013-03-10 23:27:20 +0800

    Fix js.

--- static/js/sticky.js
+++ static/js/sticky.js
@@ -3,11 +3,14 @@ function max(a, b) {
     b = parseInt(b.toString().replace('px',''));
     return a > b ? a : b;
 }
-$(function(){
+function resize() {
     w = $(window).width();
     $('.top_center,.bottom_center').css('width', (w-338) + 'px');
     $('.center_center').css('width', (w-78) + 'px');
     h = max($(window).height(), $('.center_center').css('height'))
     $('.rim_center').css('height', (h-76) + 'px');
-
+}
+$(function(){
+    resize();
+    $(window).resize(resize);
 });
